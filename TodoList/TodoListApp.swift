d10554e11c4941c32bd13b14e9d8c53c60a73e66
Daniel <danielbasaldua03@gmail.com>
2022-12-26 18:44:00 -0700

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Daniel Basaldua on 12/26/22.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
